00be961176c585f8309dc1716d8efd8236103027
Jesse Sosmena <diximobureros@gmail.com>
2018-03-16 14:59:53 +0800

    Update app title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Itm Mobile Shop</title>
+    <title>e-Commerce Laravel 5</title>
 
     <!-- Styles -->
     <link href="/css/app.css" rel="stylesheet">
